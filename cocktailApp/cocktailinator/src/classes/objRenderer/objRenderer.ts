0160b0482e0f044636d8733ea9f5e2971d8ebb09
DESKTOP-OH4MRFL\Louis Venhoff <Louis.venhoff@gmx.net>
2023-06-13 22:45:10 +0200

    Added 3d renderer for preview

---
+++ cocktailApp/cocktailinator/src/classes/objRenderer/objRenderer.ts
@@ -0,0 +1,154 @@
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import * as Three from "three";
+
+
+export type MeshSource = {
+    objectPath:string,
+    texturePath:string,
+    cameraDist:number
+}
+
+
+export enum Animation {
+    STATIC,
+    ORBITAL,
+}
+
+class ObjRenderer {
+
+    private scene: any;
+    private camera: any;
+    private renderer: any;
+    private width: number;
+    private height: number;
+    private ratio: number;
+
+    private outElement: any;
+    private renderCanvas: any;
+
+    private objSource:MeshSource;
+
+    private object: any;
+
+    private animationType:Animation;
+    
+
+    constructor(width: number, height: number, objSource:MeshSource,animation:Animation, canvas: any) {
+        this.height = height;
+        this.width = width;
+        this.objSource = objSource;
+        this.animationType = animation;
+        this.ratio = width / height;
+        this.renderCanvas = canvas;
+        this.scene = new Three.Scene();
+        this.camera = new Three.PerspectiveCamera(75, this.ratio, 0.1, 1000);
+        this.renderer = new Three.WebGLRenderer({ alpha: true, antialias: true });
+        this.sceneSetup();
+        this.addFileToScene(this.objSource);
+        //this.addExampleCube();
+        //this.addTestObj();
+
+    }
+
+    private sceneSetup() {
+        this.renderer.setSize(this.width, this.height);
+        this.outElement = this.renderer.domElement;
+
+        // gBlack Background for debugging!
+        // let context:any = this.renderCanvas.getContext("2d");
+        // context.fillStyle = 'rgba(0,0,0,0.5)';
+        // context.fillRect(0,0,window.innerWidth,window.innerHeight);
+
+        // this.renderer.render(this.scene, this.camera);
+    }
+
+    private render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+
+
+    private async loadMesh(mesh: MeshSource): Promise<any> {
+
+        const loader = new OBJLoader();
+    
+        
+        return new Promise((resolve, reject) => {
+
+            loader.load(mesh.objectPath, (obj: any) => { resolve(obj.children[0].geometry) });
+
+        });
+
+    }
+
+    private async loadTextures(mesh :MeshSource):Promise<any>
+    {
+        const loader = new Three.TextureLoader();
+
+
+        const material = new Three.MeshBasicMaterial({
+            map: loader.load(mesh.texturePath)
+        });
+
+        return new Promise((resolve, reject) => {
+            resolve(material);
+        })
+    }
+
+    public async addFileToScene(mesh:MeshSource) {
+        
+       
+        const geo = await this.loadMesh(mesh);
+        
+        const material = await this.loadTextures(mesh);
+
+        this.object = new Three.Mesh(geo,material);
+        this.object.position.y -= 0.1;
+        this.scene.add(this.object);
+        this.camera.position.z = mesh.cameraDist;
+        this.renderCanvas.appendChild(this.outElement);
+        console.log(this.renderCanvas);
+        this.render();
+        this.rotateObject();
+    }
+
+    private enableOrbitControls(){
+
+    }
+
+    
+    
+    
+    private async rotateObject() {
+        while (true) {
+            this.object.rotation.y += 0.1;
+            //this.object.rotation.x += 0.1;
+            this.render();
+            await this.sleep(75);
+        }
+    }
+
+    private async sleep(time: number): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => { resolve(true) }, time)
+        })
+    }
+
+
+    private animate()
+    {
+        requestAnimationFrame(this.animate)
+
+
+
+        this.render();
+    }
+
+
+
+}
+
+
+
+export default ObjRenderer;
--- cocktailApp/cocktailinator/src/components/cocktailContainer/cocktailContainer.tsx
+++ cocktailApp/cocktailinator/src/components/cocktailContainer/cocktailContainer.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./cocktailContainerStyle.css";
-
+import ObjRenderer from "../../classes/objRenderer/objRenderer";
+import { MeshSource, Animation } from "../../classes/objRenderer/objRenderer";
 
 const CocktailContainer:React.FC = () => 
 {
+   
+    const canvasElement = useRef<any>();
+    const [sceneRenderer, setSceneRenderer] = useState<ObjRenderer>();
+
+    useEffect(() => {
+        if(canvasElement.current != undefined)
+        {
+            let obj:MeshSource = {objectPath:"/assets/r2d2/r2-d2.obj", texturePath:"/assets/r2d2/R2D2_Textures.jpg", cameraDist:200}
+            //let obj:MeshSource = {objectPath:"/assets/gear/Gear.obj", texturePath:"/assets/gear/Gear_Texture.png", cameraDist:20}
+            setSceneRenderer(new ObjRenderer(120, 120,obj, Animation.STATIC, canvasElement.current));
+        }
+    },[canvasElement]);
+
+
+
     return(
     <div className="cocktailContainerMainDiv">
+        <div className="objCanvas" ref={canvasElement}>
 
+        </div>
     </div>
     )
 }
